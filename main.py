4a292103631431fe1540a717fb82db559a4c2b43
slizky <53127582+slizky@users.noreply.github.com>
2021-08-02 01:35:59 +0300

    new version

--- main.py
+++ main.py
@@ -10,18 +10,25 @@ from tokenVK import token
 
 
 def main():
-    name_group = input("Введите название группы: ")
-    url1 = f"https://api.vk.com/method/groups.getMembers?group_id={name_group}&offset=999&access_token=7c14770d7c14770d7c14770d257c6cda8d77c147c14770d1cece0d3e314031ab161ba64&v=5.52"
+    #name_group = input("Введите название группы: ")
+    url1 = f"https://api.vk.com/method/groups.getMembers?group_id=opt2020&offset=1070&access_token=7c14770d7c14770d7c14770d257c6cda8d77c147c14770d1cece0d3e314031ab161ba64&v=5.52"
     req = requests.get(url1)
     print(req.text)
     print(url1)
-    mass = []
     mass = req.text
     a = '['
-    for i in mass:
-        if i == a:
-            print(i)
-    f = open('file.txt', 'wt')
-    f.write(req.text)
+    b = ']'
+    c = mass.index(a)
+    d = mass.index(b)
+    mainId = mass[c+1:d]
+    print(mainId)
+    digits = []
+    digits = mainId.split(',')
+    print(digits)
+    f = open('file.txt', 'wt', encoding='utf-8')
+    for i in digits:
+        url = f"https://api.vk.com/method/users.get?user_id={i}&fields=connections&access_token=7c14770d7c14770d7c14770d257c6cda8d77c147c14770d1cece0d3e314031ab161ba64&v=5.52"
+        req = requests.get(url)
+        f.write(req.text)
     f.close()
 main()
